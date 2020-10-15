11632f8750659c54c08821dc0e708db9f291e062
Jake Nguyen <jakey@Jakes-Air.fios-router.home>
2020-10-15 12:29:48 -0400

    lecture

---
+++ student_examples/tweets/models/tweet.js
@@ -0,0 +1,23 @@
+// CONNECTION TO THE DATABASE
+const mongoose = require('../db/connection')
+
+// IMPORT THE SCHEMA CLASS
+const Schema = moongose.Schema
+
+// INSTANTIATE A NEW INSTANCE OF A SCHEMA CLASS
+const tweetSchema = new Schema(
+    {
+		title: String,
+		body: String,
+		author: String,
+		likes: { type: Number, default: 0 },
+		sponsored: { type: Boolean, default: false },
+	},
+	{ timestamps: true }
+)
+
+// CREATE THE MODEL AND ASSOCIATE IT WITH A SCHEMA
+const Tweet = mongoose.model('Tweet', tweetSchema)
+
+// EXPORT THE MODEL
+module.exports = Tweet
