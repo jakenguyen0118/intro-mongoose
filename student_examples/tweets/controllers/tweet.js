6eb84fbd67c2666c6a4ed903f57545902a6d2f6c
Jake Nguyen <jakey@Jakes-Air.fios-router.home>
2020-10-15 15:07:30 -0400

    controller complete

---
+++ student_examples/tweets/controllers/tweet.js
@@ -0,0 +1,58 @@
+// IMPORT THE DB CONNECTION
+const mongoose = require('../db/connection')
+
+// IMPORT THE TWEET MODEL
+const Tweet = require('../models/tweet')
+
+// CREATE THE DB CONNECTION IN ORDER TO CLOSE THE CONNECTION
+const db = mongoose.connection
+
+// FIND ALL - INDEX ROUTE
+const index = () => {
+    Tweet.find((error, tweets) => {
+        console.log('tweets', tweets)
+        // CLOSE THE CONNECTION
+        db.close()
+	})
+}
+
+// index()
+
+// FIND ALL - RETURN A SUBSET OF KEYS
+const indexSubset = () => {
+    Tweet.find({}, 'title body', (err, tweets) => {
+        console.log('tweets', tweets)
+        db.close()
+    })
+}
+
+// indexSubset()
+
+// FIND ONE - SHOW ROUTE
+const show = () => {
+    Tweet.find({title: 'Water'}, (err, tweet) => {
+        console.log('tweets - find one', tweet)
+        db.close()
+    })
+}
+
+// show()
+
+const likes20 = () => {
+    Tweet.find( { likes: {$gte: 20 } }, (err, tweets) => {
+        console.log(tweets)
+        db.close()
+    })
+}
+
+// likes20()
+
+const likes20AndAlex = () => {
+	Tweet.find({ likes: { $gte: 20 }, author: /alex/i }, (err, tweets) => {
+		console.log(tweets)
+		db.close()
+	})
+}
+
+// likes20AndAlex()
+
