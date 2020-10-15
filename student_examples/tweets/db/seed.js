60fa1db355c4def001d4ce157dd019b636f73a3c
Jake Nguyen <jakey@Jakes-Air.fios-router.home>
2020-10-15 13:25:05 -0400

    refractored for promises and startup script

--- student_examples/tweets/db/seed.js
+++ student_examples/tweets/db/seed.js
@@ -10,16 +10,24 @@ const manyTweets = require('./seedData.json')
 // CREATE THE DB CONNECTION
 const db = mongoose.connection
 
-// CREATE ALL THE TWEETS
-Tweet.insertMany(manyTweets, (error, tweets) => {
-	if (error) {
-		console.log('error', error)
-	} else {
+// CREATE ALL TWEETS USING .THEN()
+Tweet.deleteMany({}).then(() => {
+	Tweet.insertMany(manyTweets).then((tweets) => {
 		console.log('tweets', tweets)
-	}
-	// CLOSE THE CONNECTION TO THE DB
-	db.close()
+		db.close()
+	})
 })
+
+// CREATE ALL THE TWEETS USING A CALLBACK
+// Tweet.insertMany(manyTweets, (error, tweets) => {
+// 	if (error) {
+// 		console.log('error', error)
+// 	} else {
+// 		console.log('tweets', tweets)
+// 	}
+// 	// CLOSE THE CONNECTION TO THE DB
+// 	db.close()
+// })
 
 // OUR FIRST TWEET
 // const myFirstTweet = {
